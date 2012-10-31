f5714bb0dd24f10395f3c272e4fbe44b0da46610
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2012-10-31 15:14:43 +0100

    introduced a dice abstraction

--- spec/DieSpec.js
+++ spec/DieSpec.js
@@ -7,6 +7,15 @@ describe("A Die", function(){
         expect(sides).toBe(6);
     });
     
+    it("should return a value between 1 and number of sides (inclusive) when cast", function(){
+        var die = new GURPS.Die({ sides : 3 });
+
+        var value = die.cast();
+        
+        expect(value).toBeGreaterThan(0);
+        expect(value).toBeLessThan(die.sides() + 1);
+    });
+    
     it("should signal a value between 1 and number of sides (inclusive) when cast", function(){
 	var value = undefined;
         var die = new GURPS.Die({ sides : 3 });
@@ -18,5 +27,26 @@ describe("A Die", function(){
         
         expect(value).toBeGreaterThan(0);
         expect(value).toBeLessThan(die.sides() + 1);
+    });
+});
+
+describe("Dice", function(){
+    it("should hold a number of die", function(){
+	var dice = new GURPS.Dice();
+
+	dice.add(new GURPS.Die());
+
+	expect(dice.size()).toBe(1);
+    });
+
+    it("should be able to be cast", function(){
+	var sides = 6
+	var dice = new GURPS.Dice();
+	dice.add(new GURPS.Die({ sides : sides }));
+
+        var value = dice.cast();
+        
+        expect(value).toBeGreaterThan(0);
+        expect(value).toBeLessThan(sides + 1);
     });
 });
--- src/Die.js
+++ src/Die.js
@@ -7,9 +7,24 @@
         },
         
         cast : function(){
-	    this.trigger("cast", Math.floor((this.get("random")() * this.sides()) + 1));
+	    var result = Math.floor((this.get("random")() * this.sides()) + 1);
+	    this.trigger("cast", result);
+	    return result;
         }
     });
 
-    GURPS.Die = Die;    
+    var Dice = Backbone.Collection.extend({
+	model : Die,
+
+	cast : function(){
+	    var result = 0;
+	    this.each(function(die){
+		result += die.cast();
+	    });
+	    return result;
+	}
+    });
+
+    GURPS.Die = Die;
+    GURPS.Dice = Dice;
 })(jQuery, _, Backbone, GURPS);
